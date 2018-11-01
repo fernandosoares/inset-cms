b5960912b826be7892ea43464968466e7da1fd47
Fernando Soares <fernandosoares.dev@gmail.com>
2018-11-01 14:34:59 -0300

    DB initial config

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize')
+
+const database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 1000
+    },
+    operatorsAliases: false
+})
+
+module.exports = database
